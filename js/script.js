b14b3310db43d67f268aa152c07a15960410dcfc
AbhaySinghNaruka <abhaynaruka.asn@gmail.com>
2022-10-20 10:11:12 +0200

    ortografia corrected, again

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let nome = prompt('Qual è il tuo nome?');
-let cognome = prompt('Quanl è il tuo cognome?');
+let cognome = prompt('Qual è il tuo cognome?');
 let colorePreferito = prompt('Qual è il tuo colore preferito?');
 
 document.getElementById('year').innerHTML= nome + cognome + colorePreferito + 22;
