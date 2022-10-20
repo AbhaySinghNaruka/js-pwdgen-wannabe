2ea7a6c7ad3afe40b0dafe4a94eab0352cde4bf3
AbhaySinghNaruka <abhaynaruka.asn@gmail.com>
2022-10-20 08:52:41 +0200

    completed

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let nome = prompt('Quale è il tuo nome?');
+let cognome = prompt('Quanl è il tuo cognome?');
+let colorePreferito = prompt('Qual è il tuo colore preferito?');
+
+document.getElementById('year').innerHTML= nome + cognome + colorePreferito + 22;
